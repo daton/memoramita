e94393401b3990f3a1343794266753be4961eccb
Juan Carlos Campos <rapidclimate@gmail.com>
2018-07-17 13:41:55 -0500

    Add files via upload

---
+++ TarjetaMemorama.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TarjetaMemorama : MonoBehaviour {
+    public GameObject cartaAbajo;
+    public EscenaControlador controlador;
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+      
+    }
+
+    public void SetTarjeta(int id, Sprite imagen)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = imagen;
+
+    }
+
+
+  
+    public void OnMouseDown()
+    {
+        print("probando tarjeta");
+        if (cartaAbajo.activeSelf)
+        {
+            cartaAbajo.SetActive(false);
+        }
+    }
+}
